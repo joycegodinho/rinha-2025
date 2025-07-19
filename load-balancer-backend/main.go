162496883569b52a622ac0eda3525d524014a354
JOYCE GODINHO <joyce@origemmotos.com.br>
2025-07-19 07:29:45 -0300

    feat/added fast http to database

--- load-balancer-backend/main.go
+++ load-balancer-backend/main.go
@@ -2,56 +2,48 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"net/http/httputil"
 	"os"
 	"sync/atomic"
+
+	"github.com/valyala/fasthttp"
 )
 
 type LoadBalancer struct {
-	proxies         []Server
+	servers         []string
 	roundRobinCount uint64
+	client          *fasthttp.Client
 }
 
-type Server struct {
-	proxy *httputil.ReverseProxy
-}
-
-func (lb *LoadBalancer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (lb *LoadBalancer) Handler(ctx *fasthttp.RequestCtx) {
 	next := lb.nextIndex()
-	lb.proxies[next].proxy.ServeHTTP(w, r)
+
+	req := &ctx.Request
+	resp := &ctx.Response
+
+	req.SetHost(lb.servers[next])
+
+	if err := lb.client.Do(req, resp); err != nil {
+		ctx.Error(err.Error(), fasthttp.StatusBadGateway)
+	}
 }
 
 func (lb *LoadBalancer) nextIndex() int {
-	return int(atomic.AddUint64(&lb.roundRobinCount, 1) % uint64(len(lb.proxies)))
-}
-
-func newReverseProxy(target string) *httputil.ReverseProxy {
-	proxy := &httputil.ReverseProxy{
-		Director: func(r *http.Request) {
-			r.URL.Scheme = "http"
-			r.URL.Host = target
-		},
-		Transport: &http.Transport{
-			MaxIdleConns:        100,
-			MaxIdleConnsPerHost: 100,
-			MaxConnsPerHost:     100,
-		},
-	}
-	return proxy
+	return int(atomic.AddUint64(&lb.roundRobinCount, 1) % uint64(len(lb.servers)))
 }
 
 func main() {
-	servers := []Server{
-		{proxy: newReverseProxy(os.Getenv("FIRST_SERVER_HOST") + ":" + os.Getenv("FIRST_SERVER_PORT"))},
-		{proxy: newReverseProxy(os.Getenv("SECOND_SERVER_HOST") + ":" + os.Getenv("SECOND_SERVER_PORT"))},
+	servers := []string{
+		os.Getenv("FIRST_SERVER_HOST") + ":" + os.Getenv("FIRST_SERVER_PORT"),
+		os.Getenv("SECOND_SERVER_HOST") + ":" + os.Getenv("SECOND_SERVER_PORT"),
 	}
 	lb := &LoadBalancer{
-		proxies:         servers,
+		servers:         servers,
 		roundRobinCount: 0,
+		client:          &fasthttp.Client{},
 	}
 
-	fmt.Println("Starting GO LOAD BALANCER service on port: " + os.Getenv("PORT"))
+	port := os.Getenv("PORT")
+	fmt.Println("Starting GO LOAD BALANCER service on port: " + port)
 
-	http.ListenAndServe(":"+os.Getenv("PORT"), lb)
+	fasthttp.ListenAndServe(":"+port, lb.Handler)
 }
