b0632dcc9004c7af6618348eb323ea66dc0ad0f9
JOYCE GODINHO <joyce@origemmotos.com.br>
2025-07-31 14:40:12 -0300

    feat/added select processor with lag

--- payments-service/handler/payment.go
+++ payments-service/handler/payment.go
@@ -67,6 +67,30 @@ func PaymentHandler(defaultChecker, fallbackChecker *health.HealthManager) fasth
 	}
 }
 
+const (
+	gracefulLagMs = 100 // Allow default to be up to 100ms slower than fallback
+)
+
+// Select processor by time with graceful lag
+func SelectProcessor(defaultHealth, fallbackHealth *health.ProcessorHealth) string {
+	if defaultHealth == nil && fallbackHealth == nil {
+		return ""
+	}
+	if defaultHealth.Failing && fallbackHealth.Failing {
+		return ""
+	}
+	if !defaultHealth.Failing && fallbackHealth.Failing {
+		return "default"
+	}
+	if defaultHealth.Failing && !fallbackHealth.Failing {
+		return "fallback"
+	}
+	if defaultHealth.MinResponseTime <= fallbackHealth.MinResponseTime+gracefulLagMs {
+		return "default"
+	}
+	return "fallback"
+}
+
 // Select processor by time
 // func SelectProcessor(defaultHealth, fallbackHealth *health.ProcessorHealth) string {
 // 	if defaultHealth == nil && fallbackHealth == nil {
@@ -81,7 +105,6 @@ func PaymentHandler(defaultChecker, fallbackChecker *health.HealthManager) fasth
 // 	if defaultHealth.Failing && !fallbackHealth.Failing {
 // 		return "fallback"
 // 	}
-
 // 	if defaultHealth.MinResponseTime <= fallbackHealth.MinResponseTime {
 // 		return "default"
 // 	}
@@ -89,20 +112,6 @@ func PaymentHandler(defaultChecker, fallbackChecker *health.HealthManager) fasth
 // }
 
 // Select default if on
-func SelectProcessor(defaultHealth, fallbackHealth *health.ProcessorHealth) string {
-	if defaultHealth == nil && fallbackHealth == nil {
-		return ""
-	}
-	if defaultHealth.Failing && fallbackHealth.Failing {
-		return ""
-	}
-	if !defaultHealth.Failing {
-		return "default"
-	}
-	return "fallback"
-}
-
-// Select processor by score
 // func SelectProcessor(defaultHealth, fallbackHealth *health.ProcessorHealth) string {
 // 	if defaultHealth == nil && fallbackHealth == nil {
 // 		return ""
@@ -110,17 +119,10 @@ func SelectProcessor(defaultHealth, fallbackHealth *health.ProcessorHealth) stri
 // 	if defaultHealth.Failing && fallbackHealth.Failing {
 // 		return ""
 // 	}
-// 	if !defaultHealth.Failing && fallbackHealth.Failing {
+// 	if !defaultHealth.Failing {
 // 		return "default"
 // 	}
-// 	if defaultHealth.Failing && !fallbackHealth.Failing {
-// 		return "fallback"
-// 	}
-
-// 	if 3*fallbackHealth.MinResponseTime < defaultHealth.MinResponseTime {
-// 		return "fallback"
-// 	}
-// 	return "default"
+// 	return "fallback"
 // }
 
 func markProcessorAsFailing(proc string, d *health.HealthManager, f *health.HealthManager) {
